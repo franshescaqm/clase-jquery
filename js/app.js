6ff088d9789881a218b48994ea15f1f649ba2e35
franshescaqm <franshesca_94@hotmail.com>
2017-12-19 18:24:45 -0500

    Clase parctica de jquery

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/* first exercise regarding Jquery */
+
+/*$(document).ready(
+    function() {
+    console.log($('#p-text').text());
+    //console.log(jQuery('.text').html());
+    ($('.text').html('Hola! aqui estoy'));
+    console.log($('.input-class').val());
+    }
+)*/
