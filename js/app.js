830e3101c30e0d09d1a7cafc09c0f4ee58ff2519
franshescaqm <franshesca_94@hotmail.com>
2017-12-20 11:57:08 -0500

    Adding first-child element red

--- js/app.js
+++ js/app.js
@@ -8,3 +8,18 @@
     console.log($('.input-class').val());
     }
 )*/
+
+/* Example with first-child : turn into red */
+
+$(document).ready(function() {
+$('.students > li:first-child').on('click', function() {
+$(this).css( 'background-color', 'red' );
+});
+});
+
+/*background Verde*/
+/*$(document).ready(function() {
+    $('.students:first-child , ul').on('click', function() {
+    $(this).css( 'background-color', 'green' );
+    });
+    });*/
